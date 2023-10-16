c22b6fcbdae990cd58f529f77ed34e7f623505d8
Fibo4487 <fibonacci2246@gmail.com>
2023-10-16 19:32:26 +0900

    Feat: next-ui 라이브러리 추가 및 이미지 업로드 리팩토링

---
+++ front/src/app/RootLayout.tsx
@@ -0,0 +1,15 @@
+import { inter } from "./layout";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}

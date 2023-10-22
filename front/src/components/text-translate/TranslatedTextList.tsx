fe48d99d4b825760b68f61354d7ac3b7ffc1fa80
Fibo4487 <fibonacci2246@gmail.com>
2023-10-23 02:30:11 +0900

    feat/text-translate 텍스트 번역페이지 WIP

---
+++ front/src/components/text-translate/TranslatedTextList.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  getKeyValue,
+} from "@nextui-org/react";
+import { useTextTranslateValue } from "./textTranslateProvider";
+import { useEffect } from "react";
+
+export default function TranslatedTextList() {
+  const columns = [
+    { key: "language", label: "Language" },
+    { key: "input", label: "Input" },
+    { key: "output", label: "Output" },
+  ];
+
+  const { translatedTextList } = useTextTranslateValue();
+
+  useEffect(() => {
+    console.log(translatedTextList, "translatedTextList");
+  }, [translatedTextList]);
+  return (
+    <Table aria-label="Example table with dynamic content">
+      <TableHeader>
+        {columns.map((column) => (
+          <TableColumn key={column.key}>{column.label}</TableColumn>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {translatedTextList.map((row) => (
+          <TableRow key={row.key}>
+            {(columnKey) => (
+              <TableCell>{getKeyValue(row, columnKey)}</TableCell>
+            )}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}

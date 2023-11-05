9cf32d687d0588dbc2c96bac4e95bf1c50acf7ab
imbel9830 <imbel9830@gmail.com>
2023-11-05 11:35:38 +0900

    implementation: lombok

--- back/src/main/java/com/icebear/mintBear/Domain/imgVO.java
+++ back/src/main/java/com/icebear/mintBear/Domain/imgVO.java
@@ -1,8 +1,13 @@
 package com.icebear.mintBear.Domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@NoArgsConstructor
+@Getter
+@NonNull
 public class imgVO {
     private String url;
 
-    public String getUrl() {
-        return url;
-    }
 }

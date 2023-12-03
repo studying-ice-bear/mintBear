09639c11285cf89c8a97908aa2cc7a6b0a9f0e30
Fibo4487 <fibonacci2246@gmail.com>
2023-12-03 14:20:45 +0900

    typeerror fix

---
+++ front/src/components/text-translate/languageSelect.tsx
@@ -0,0 +1,48 @@
+"use client";
+import {
+  Select,
+  SelectSection,
+  SelectItem,
+  SelectProps,
+} from "@nextui-org/react";
+import {
+  useTextTranslateAction,
+  useTextTranslateValue,
+} from "./textTranslateProvider";
+import { ChangeEventHandler } from "react";
+import { LANGUAGE_OPTIONS } from "@/i18n-config";
+
+interface LanguageSelectProps extends Omit<SelectProps, "children"> {}
+
+export default function LanguageSelect({ ...rest }: LanguageSelectProps) {
+  const { selectedLanguage } = useTextTranslateValue();
+  const { languageChanged } = useTextTranslateAction();
+
+  const onSelectLanguage: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    languageChanged(event.target.value);
+    console.log(selectedLanguage);
+  };
+
+  const selectOptions = Object.entries(LANGUAGE_OPTIONS).map(
+    ([key, value]) => ({
+      label: value,
+      value: key,
+    })
+  );
+
+  return (
+    <Select
+      aria-label="Language"
+      label="Select Language to translate"
+      value={selectedLanguage}
+      onChange={onSelectLanguage}
+      {...rest}
+    >
+      {selectOptions.map((option) => (
+        <SelectItem key={option.value} value={option.value}>
+          {option.label}
+        </SelectItem>
+      ))}
+    </Select>
+  );
+}

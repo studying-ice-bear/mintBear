13930d3074a3d153b8de89e1e550a3fa95af8010
Fibo4487 <fibonacci2246@gmail.com>
2023-12-25 19:43:14 +0900

    fix module not found error

---
+++ front/src/components/image-upload/ImageCrop.tsx
@@ -0,0 +1,212 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+} from "@nextui-org/react";
+import React, { useState } from "react";
+import ImageEditor from "./imageEditor";
+import Cropper, { Area } from "react-easy-crop";
+import useParser, { OCRLangOption } from "@/app/store/useParser";
+import { firebaseStorage } from "@/config/firebaseStorage";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { Locale } from "@/i18n-config";
+
+const ImageCropModal = ({
+  isOpen,
+  onOpenChange,
+  lng,
+}: {
+  isOpen: boolean;
+  onOpenChange: (isOpen: boolean) => void;
+  lng: Locale;
+}) => {
+  const { imageUrl, setImageUrl, getParsing, option } = useParser();
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+  const [rotation, setRotation] = useState(0);
+
+  const onCropComplete: (
+    croppedArea: Area,
+    croppedAreaPixels: Area
+  ) => void = async (croppedArea, croppedAreaPixels) => {
+    try {
+      const croppedImg = await getCroppedImg(
+        imageUrl!,
+        croppedAreaPixels,
+        rotation
+      );
+      const fileName = new URL(imageUrl!).pathname.split("/").pop();
+      const storageRef = ref(firebaseStorage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, croppedImg!);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // const progress =
+          //   (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          // setUploadProgress(progress);
+        },
+        (error) => {
+          // setUploadProgress(null);
+        },
+        async () => {
+          try {
+            const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+            setImageUrl(downloadURL);
+            await getParsing({
+              url: downloadURL,
+              option: option ?? OCRLangOption[lng],
+            });
+          } catch (error) {
+            setImageUrl(null);
+          }
+        }
+      );
+    } catch (e) {
+      console.error(e);
+      setImageUrl(null);
+    }
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+      isDismissable={false}
+      size="5xl"
+      className="min-h-[100vh]"
+    >
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalBody className="min-h-[95vh] overflow-auto flex items-center justify-center">
+              {imageUrl !== null && (
+                <Cropper
+                  image={imageUrl}
+                  crop={crop}
+                  zoom={zoom}
+                  rotation={rotation}
+                  aspect={4 / 3}
+                  onCropChange={setCrop}
+                  onCropComplete={onCropComplete}
+                  onZoomChange={setZoom}
+                  onRotationChange={setRotation}
+                />
+              )}
+            </ModalBody>
+            <ModalFooter>
+              <Button color="danger" variant="light" onPress={onClose}>
+                닫기
+              </Button>
+              <Button color="primary" onPress={onClose}>
+                저장
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ImageCropModal;
+
+export const createImage = (url: string): Promise<HTMLImageElement> =>
+  new Promise((resolve, reject) => {
+    const image = new Image();
+    image.addEventListener("load", () => resolve(image));
+    image.addEventListener("error", (error) => reject(error));
+    image.setAttribute("crossOrigin", "anonymous"); // needed to avoid cross-origin issues on CodeSandbox
+    image.src = url;
+  });
+
+export function getRadianAngle(degreeValue: number) {
+  return (degreeValue * Math.PI) / 180;
+}
+
+/**
+ * Returns the new bounding area of a rotated rectangle.
+ */
+export function rotateSize(width = 0, height = 0, rotation = 0) {
+  const rotRad = getRadianAngle(rotation);
+
+  return {
+    width:
+      Math.abs(Math.cos(rotRad) * width) + Math.abs(Math.sin(rotRad) * height),
+    height:
+      Math.abs(Math.sin(rotRad) * width) + Math.abs(Math.cos(rotRad) * height),
+  };
+}
+
+export async function getCroppedImg(
+  imageSrc = "",
+  pixelCrop = { x: 0, y: 0, width: 0, height: 0 },
+  rotation = 0,
+  flip = { horizontal: false, vertical: false }
+): Promise<Blob | null> {
+  const image = await createImage(imageSrc);
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+
+  if (!ctx) {
+    return null;
+  }
+
+  const rotRad = getRadianAngle(rotation);
+
+  // calculate bounding box of the rotated image
+  const { width: bBoxWidth, height: bBoxHeight } = rotateSize(
+    image.width,
+    image.height,
+    rotation
+  );
+
+  // set canvas size to match the bounding box
+  canvas.width = bBoxWidth;
+  canvas.height = bBoxHeight;
+
+  // translate canvas context to a central location to allow rotating and flipping around the center
+  ctx.translate(bBoxWidth / 2, bBoxHeight / 2);
+  ctx.rotate(rotRad);
+  ctx.scale(flip.horizontal ? -1 : 1, flip.vertical ? -1 : 1);
+  ctx.translate(-image.width / 2, -image.height / 2);
+
+  // draw rotated image
+  ctx.drawImage(image, 0, 0);
+
+  const croppedCanvas = document.createElement("canvas");
+
+  const croppedCtx = croppedCanvas.getContext("2d");
+
+  if (!croppedCtx) {
+    return null;
+  }
+
+  // Set the size of the cropped canvas
+  croppedCanvas.width = pixelCrop.width;
+  croppedCanvas.height = pixelCrop.height;
+
+  // Draw the cropped image onto the new canvas
+  croppedCtx.drawImage(
+    canvas,
+    pixelCrop.x,
+    pixelCrop.y,
+    pixelCrop.width,
+    pixelCrop.height,
+    0,
+    0,
+    pixelCrop.width,
+    pixelCrop.height
+  );
+
+  // As Base64 string
+  // return croppedCanvas.toDataURL('image/jpeg');
+
+  // As a blob
+  return new Promise((resolve, reject) => {
+    croppedCanvas.toBlob((file) => {
+      resolve(file!);
+    }, "image/jpeg");
+  });
+}
